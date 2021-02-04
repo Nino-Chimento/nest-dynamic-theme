a09a63c3012ed8a3b7761dd1c72e9feaa3910d90
Nino Chimento <saninoc84@gmil.com>
2021-02-04 14:17:38 +0100

    global style

---
+++ layout/Basic.tsx
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    // this is the shared style
+    body { 
+        background-color:red;
+    }
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+h1 {
+    color: yellow !important; // the important is just to show that the style works!
+}
+
+  // anything else you would like to include
+`;
+
+const BasicLayout = ({ children }: { children: any }) => {
+  return (
+    <>
+      <GlobalStyle />
+      {children}
+    </>
+  );
+};
+
+export default BasicLayout;
