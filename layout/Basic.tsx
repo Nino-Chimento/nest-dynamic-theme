1191d76afda0589371a5787f8ae0deff2eb7316f
Nino Chimento <saninoc84@gmil.com>
2021-02-04 15:29:49 +0100

    layout global styled-components

--- layout/Basic.tsx
+++ layout/Basic.tsx
@@ -1,32 +1,64 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components"; // theme.ts
+import { DefaultTheme } from "styled-components";
+import { ThemeProvider } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`
-    // this is the shared style
-    body { 
-        background-color:red;
-    }
-  html {
-    box-sizing: border-box;
+export const theme: any = {
+  id: "T_001",
+  name: "Light",
+  colors: {
+    body: "#c53030",
+    text: "#000000",
+    button: {
+      text: "#FFFFFF",
+      background: "#000000",
+    },
+    link: {
+      text: "teal",
+      opacity: 1,
+    },
+  },
+  font: "Tinos",
+};
+
+export const GlobalStyle: any = createGlobalStyle`
+    body {
+    background: ${({ theme }) => theme.colors.body};
+    color: ${({ theme }) => theme.colors.text};
+    font-family: ${({ theme }) => theme.font};
+    transition: all 0.50s linear;
   }
 
-  *,
-  *::before,
-  *::after {
-    box-sizing: inherit;
+  a {
+    color: ${({ theme }) => theme.colors.link.text};
+    cursor: pointer;
   }
 
-h1 {
-    color: yellow !important; // the important is just to show that the style works!
-}
+  button {
+    border: 0;
+    display: inline-block;
+    padding: 12px 24px;
+    font-size: 14px;
+    border-radius: 4px;
+    margin-top: 5px;
+    cursor: pointer;
+    background-color: #1064EA;
+    color: #FFFFFF;
+    font-family: ${({ theme }) => theme.font};
+  }
 
-  // anything else you would like to include
+  button.btn {
+    background-color: ${({ theme }) => theme.colors.button.background};
+    color: ${({ theme }) => theme.colors.button.text};
+  }
 `;
 
 const BasicLayout = ({ children }: { children: any }) => {
   return (
     <>
-      <GlobalStyle />
-      {children}
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
     </>
   );
 };
